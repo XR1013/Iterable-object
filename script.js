8d02edc1cc4bf8fcb5eabaed532fcc359cfb8dc0
XR1013 <xraptor1013@gmail.com>
2024-09-20 09:23:19 +0900

    문자 분리 함수 주석 설명

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-// step2 - span요소 자체를 우리가 원하는 요소명으로 인수 전달 처리
-function splitText(elem, tag) {
-    const el = document.querySelector(elem);
-    const el_text = el.innerText;
-    // 아래와 같이 DOM.style.fontSize='문자값속성'은
-    // 실제 html 태그상에 <h1 style='font-size:0px'></h1>
-    // 와 같이 인라인 스타일 형태로 적용
-    el.style.fontSize = '0px';
-    let resultText = "";
-
-    for (let letter of el_text) {
-        // 동적으로 생성되는 문자열 자체적으로 style=' '형식으로 스타일 값 연결가능
-        resultText += `<${tag} style='display:inline-block'>${letter}</${tag}>`;
+// splitText라는 이름으로 함수 선언 이 때 파라미터로 elem, tag, interval 연결 , tag와 interval에는 전달되는 인수값이 없을 때 spanm 0값을 디폴트로 설정
+function splitText(elem, tag = "span", interval = 0) {
+    // 이때 만약 제일 중요한 인수값은 elem값이 없으면 코드가 어차피 정상적으로 동작이 안될 터이므로
+    if(!elem) {
+        // 친절하게 타 개발자에게 알려주기 위해서 error 구문으로 어떤 실수를 했는지 구체적으로 콘솔 찍어줌
+        console.error("첫 번째 인수 값은 텍스트를 가져올 DOM 선택자를 넣어야 됩니다.");
+        // 어차피 구동이 안 될 코드이므로 이 다음의 의미없는 연산처리를 막기위해 return으로 해당 함수 실행 강제 종료
+        // 만약 elem값이 있으면 해당 if문의 코드블록 자체가 무시가 될테니 return으로 중지되지 않음
+        return;
     }
 
+    // 위에 조건으로 설정한 오류사항을 피하게 되면 아래 로직을 순차적으로 실행
+    // 전달 받은 elem값으로 DOM요소 선택 후 변수에 저장
+    // console.log("elem이 없을 때 불필요한 연산 시작"); = 누락 코드 주석아님
+
+    const el = document.querySelector(elem);
+    // 이미 변수에 저장해놓은 DOM요소의 텍스값을 찾아서 el_text라는 변수에 저장
+    const el_text = el.innerText;
+
+    // 인라인요소사이의 간격 버그를 없애기 위해서 부모요소인 el에 font-size:0px을 스크립트로 자동 설정
+    el.style.fontSize = '0px';
+    // resultText라는 변수에는 앞으로 반복문을 통해 문자열로 반복돌릴 태그문자열 담길 빈 그릇
+    let resultText = "";
+    // counter라는 변수에는 앞으로 반복문을 통해 카운터 증가 값을 담을 초기 변수(앞으로 값을 담을 빈 그릇)
+    let counter = 0;
+
+    // 위에서 값을 담을 초기변수를 ㅜㄴ비했으니
+    // 아래 for of 반복으로 반복처리 (for of는 문자열을 반복돌릴 때 사용)
+    // el_text에 있는 전체 문자열이 자동으로 반복돌면서 아래 코드 블록 안에 letter라는 변수로 글자 하나씩 반복처리됨
+    for (let letter of el_text) {
+        // 미리 준비한 resultText란 빈 변수 그릇에 += 연산자로 다음과 같은 코드를 계속 쌓아나가면서 문자열을 완성
+        // <span style='display:inline-block; tansition-delay: 0.2* counter>letter</span>
+        resultText += `<${tag} style='display:inline-block; transition-delay:${
+            // 파라미터로 전달된 인터벌 간격(0.2) * 반복문 내부적으로 증가시키는 카운터 값
+            // 아래 코드로 각각의 span요소에 0s, 0.2s, 0.4s씩 증가되면 딜레이 값 설정됨
+            interval || 0 * counter++
+            // 최종적으로 반복도는 letter문자값을 <span></span> 안쪽에 꽃아넣음
+        }s'>${letter}</${tag}>`;
+    }
+
+    // 위에 반복생성하며 최종 태그 문자열이 담긴 변수 resultText값을 el요소에 innerHTML로 꽃아넣음
     el.innerHTML = resultText;
 }
 
+// 위에 정의된 함수를 토대로 splitText 함수 추출
+// 이때 우리는 h1 요소의 글자를 가지고 오도록 첫번째 인수 설정
+// 두번째 인수 span을 전달해서 h1 안쪽에서 span으로 글자 값이 감싸지도록 설정
+// 마지막으로 0.05를 전달해서 딜레이 값이 0s, 0.05s, 0.1s씩으로 인터벌 생기도록 설정
 splitText("h1", "span");
 
 
@@ -27,8 +57,6 @@ splitText("h1", "span");
 
 
 
-
-// step3 - 인터벌 시간값을 3번째 인수로 전달하면 delay값이 설정됨
 // step4 - 세번째 인수 값이 전달되지 않으면 무조건 delay값을 디폴트로 0처리
 
 // 
